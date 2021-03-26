f64921205f6bc2de0457ad2d6635818ad6e9eb4c
Michael Mersiades <fakename014@gmail.com>
2021-03-26 20:43:45 +1100

    Adds optimistic response to finishPreGame mutation

--- src/mutations/finishPreGame.ts
+++ src/mutations/finishPreGame.ts
@@ -2,12 +2,26 @@ import { gql } from '@apollo/client';
 import { Game } from '../@types/dataInterfaces';
 
 export interface FinishPreGameData {
-  finishPreGame: Game;
+  finishPreGame: Partial<Game>;
 }
 
 export interface FinishPreGameVars {
   gameId: string;
 }
+
+export const getFinishPreGameOR = (id: string) => {
+  const optimisticGame = {
+    id,
+    hasFinishedPreGame: true,
+    showFirstSession: true,
+    __typename: 'Game' as 'Game',
+  };
+
+  return {
+    __typename: 'Mutation',
+    finishPreGame: optimisticGame,
+  };
+};
 
 const FINISH_PRE_GAME = gql`
   mutation FinishPreGame($gameId: String!) {
