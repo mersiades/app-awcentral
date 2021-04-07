da7e176eab00ca3d36c749fe17140852b5f9f393
Michael Mersiades <fakename014@gmail.com>
2021-04-07 21:08:25 +1000

    Refactors harm clock

--- src/components/playbookPanel/HarmBox.tsx
+++ src/components/playbookPanel/HarmBox.tsx
@@ -2,7 +2,8 @@ import React, { FC } from 'react';
 import { Box, CheckBox } from 'grommet';
 
 import CollapsiblePanelBox from '../CollapsiblePanelBox';
-import { accentColors, brandColor, TextWS } from '../../config/grommetConfig';
+import { getCircle, getSector, oclock12, oclock3, oclock6, oclock9 } from '../HarmClock';
+import { TextWS } from '../../config/grommetConfig';
 import { HarmInput } from '../../@types';
 import { CharacterHarm } from '../../@types/dataInterfaces';
 
@@ -13,107 +14,7 @@ interface HarmBoxProps {
 }
 
 const HarmBox: FC<HarmBoxProps> = ({ harm, settingHarm, handleSetHarm }) => {
-  const highlightColor = harm.isStabilized ? accentColors[0] : brandColor;
-
-  const circle = {
-    position: 'relative' as 'relative',
-    height: '200px',
-    width: '200px',
-    overflow: 'hidden',
-    borderRadius: '50%',
-  };
-
-  const sectorBase = {
-    position: 'absolute' as 'absolute',
-    left: '50%',
-    bottom: '50%',
-    height: '100%',
-    width: '100%',
-    transformOrigin: 'bottom left',
-    borderColor: '#000',
-    borderWidth: 1,
-    borderStyle: 'solid',
-    cursor: 'pointer',
-  };
-
-  const sector0 = (value: number) => {
-    return {
-      ...sectorBase,
-      backgroundColor: value > 0 ? highlightColor : '#FFF',
-      transform: 'rotate(0deg) skewY(0deg)',
-    };
-  };
-
-  const sector1 = (value: number) => {
-    return {
-      ...sectorBase,
-      backgroundColor: value > 1 ? highlightColor : '#FFF',
-      transform: 'rotate(90deg) skewY(0deg)',
-    };
-  };
-
-  const sector2 = (value: number) => {
-    return {
-      ...sectorBase,
-      backgroundColor: value > 2 ? highlightColor : '#FFF',
-      transform: 'rotate(180deg) skewY(0deg)',
-    };
-  };
-
-  const sector3 = (value: number) => {
-    return {
-      ...sectorBase,
-      backgroundColor: value > 3 ? highlightColor : '#FFF',
-      transform: 'rotate(270deg) skewY(-60deg)',
-    };
-  };
-
-  const sector4 = (value: number) => {
-    return {
-      ...sectorBase,
-      backgroundColor: value > 4 ? highlightColor : '#FFF',
-      transform: 'rotate(300deg) skewY(-60deg)',
-    };
-  };
-
-  const sector5 = (value: number) => {
-    return {
-      ...sectorBase,
-      backgroundColor: value > 5 ? highlightColor : '#FFF',
-      transform: 'rotate(330deg) skewY(-60deg)',
-    };
-  };
-
-  const oclockBase = {
-    position: 'absolute' as 'absolute',
-    padding: '3px',
-    width: '30px',
-    height: '30px',
-  };
-
-  const oclock12 = {
-    ...oclockBase,
-    top: 0,
-    left: 'calc(50% - 15px)',
-  };
-
-  const oclock3 = {
-    ...oclockBase,
-    top: 'calc(50% - 15px)',
-    right: 0,
-  };
-
-  const oclock6 = {
-    ...oclockBase,
-    bottom: -3,
-    left: 'calc(50% - 15px)',
-  };
-
-  const oclock9 = {
-    ...oclockBase,
-    top: 'calc(50% -15px)',
-    left: 0,
-  };
+  const circle = getCircle(200);
 
   const setHarmValue = (sector: number) => {
     let newValue = harm.value;
@@ -154,12 +55,36 @@ const HarmBox: FC<HarmBoxProps> = ({ harm, settingHarm, handleSetHarm }) => {
           </Box>
 
           <div style={circle}>
-            <div data-testid="harm-sector-0" style={sector0(harm.value)} onClick={() => !settingHarm && setHarmValue(0)} />
-            <div data-testid="harm-sector-1" style={sector1(harm.value)} onClick={() => !settingHarm && setHarmValue(1)} />
-            <div data-testid="harm-sector-2" style={sector2(harm.value)} onClick={() => !settingHarm && setHarmValue(2)} />
-            <div data-testid="harm-sector-3" style={sector3(harm.value)} onClick={() => !settingHarm && setHarmValue(3)} />
-            <div data-testid="harm-sector-4" style={sector4(harm.value)} onClick={() => !settingHarm && setHarmValue(4)} />
-            <div data-testid="harm-sector-5" style={sector5(harm.value)} onClick={() => !settingHarm && setHarmValue(5)} />
+            <div
+              data-testid="harm-sector-0"
+              style={getSector(harm.value, 0, 0, 0, harm.isStabilized)}
+              onClick={() => !settingHarm && setHarmValue(0)}
+            />
+            <div
+              data-testid="harm-sector-1"
+              style={getSector(harm.value, 1, 90, 0, harm.isStabilized)}
+              onClick={() => !settingHarm && setHarmValue(1)}
+            />
+            <div
+              data-testid="harm-sector-2"
+              style={getSector(harm.value, 2, 180, 0, harm.isStabilized)}
+              onClick={() => !settingHarm && setHarmValue(2)}
+            />
+            <div
+              data-testid="harm-sector-3"
+              style={getSector(harm.value, 3, 270, -60, harm.isStabilized)}
+              onClick={() => !settingHarm && setHarmValue(3)}
+            />
+            <div
+              data-testid="harm-sector-4"
+              style={getSector(harm.value, 4, 300, -60, harm.isStabilized)}
+              onClick={() => !settingHarm && setHarmValue(4)}
+            />
+            <div
+              data-testid="harm-sector-5"
+              style={getSector(harm.value, 5, 330, -60, harm.isStabilized)}
+              onClick={() => !settingHarm && setHarmValue(5)}
+            />
           </div>
         </Box>
         <Box flex="grow" pad="12px" gap="12px" justify="center">
