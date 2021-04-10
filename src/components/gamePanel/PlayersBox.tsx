c7db0bad4a6419034b69f4cad674348ba3a37eb3
Michael Mersiades <fakename014@gmail.com>
2021-04-10 15:01:12 +1000

    Adds remove player warning dialog

--- src/components/gamePanel/PlayersBox.tsx
+++ src/components/gamePanel/PlayersBox.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Box } from 'grommet';
 import { Trash } from 'grommet-icons';
 
@@ -8,8 +8,11 @@ import { useGame } from '../../contexts/gameContext';
 import { useMutation } from '@apollo/client';
 import REMOVE_PLAYER, { getRemovePlayerOR, RemovePlayerData, RemovePlayerVars } from '../../mutations/removePlayer';
 import Spinner from '../Spinner';
+import WarningDialog from '../dialogs/WarningDialog';
 
 const PlayersBox: FC = () => {
+  // -------------------------------------------------- Component state ---------------------------------------------------- //
+  const [showRemovePlayerDialog, setShowRemovePlayerDialog] = useState('');
   // ------------------------------------------------------- Hooks --------------------------------------------------------- //
   const { game, allPlayerGameRoles } = useGame();
 
@@ -25,8 +28,10 @@ const PlayersBox: FC = () => {
           variables: { gameId: game.id, playerId },
           optimisticResponse: getRemovePlayerOR(game, playerId),
         });
+        setShowRemovePlayerDialog('');
       } catch (error) {
         console.error(error);
+        setShowRemovePlayerDialog('');
       }
     }
   };
@@ -56,7 +61,7 @@ const PlayersBox: FC = () => {
             </Box>
             <Box align="end" fill>
               {!removingPlayer ? (
-                <Trash color="accent-1" onClick={() => handleRemovePlayer(player.id)} cursor="grab" />
+                <Trash color="accent-1" onClick={() => setShowRemovePlayerDialog(player.id)} cursor="pointer" />
               ) : (
                 <Spinner />
               )}
@@ -68,17 +73,28 @@ const PlayersBox: FC = () => {
   };
 
   return (
-    <CollapsiblePanelBox open title="Players">
-      <Box
-        fill="horizontal"
-        justify="between"
-        align="start"
-        gap="12px"
-        animation={{ type: 'fadeIn', delay: 0, duration: 500, size: 'xsmall' }}
-      >
-        {renderPlayers()}
-      </Box>
-    </CollapsiblePanelBox>
+    <>
+      {!!showRemovePlayerDialog && (
+        <WarningDialog
+          title="Remove player?"
+          text="This cannot be undone."
+          buttonTitle="REMOVE"
+          handleClose={() => setShowRemovePlayerDialog('')}
+          handleConfirm={() => handleRemovePlayer(showRemovePlayerDialog)}
+        />
+      )}
+      <CollapsiblePanelBox open title="Players">
+        <Box
+          fill="horizontal"
+          justify="between"
+          align="start"
+          gap="12px"
+          animation={{ type: 'fadeIn', delay: 0, duration: 500, size: 'xsmall' }}
+        >
+          {renderPlayers()}
+        </Box>
+      </CollapsiblePanelBox>
+    </>
   );
 };
 
