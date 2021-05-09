27b51c006bc5f1b9cc3de90e1c6a6e08d8d53207
Michael Mersiades <fakename014@gmail.com>
2021-05-09 19:38:22 +1000

    Adds setDeathMoves mutation

---
+++ src/mutations/setDeathMoves.ts
@@ -0,0 +1,93 @@
+import { gql } from '@apollo/client';
+import { Character } from '../@types/dataInterfaces';
+import { MoveType, PlaybookType } from '../@types/enums';
+
+export interface SetDeathMovesData_CharacterMove {
+  id: string;
+  isSelected: boolean;
+  name: string;
+  kind: MoveType;
+  description: string;
+  playbook: PlaybookType;
+  __typename?: 'CharacterMove';
+}
+
+export interface SetDeathMovesData {
+  setDeathMoves: {
+    id: string;
+    playbook: PlaybookType;
+    characterMoves: SetDeathMovesData_CharacterMove[];
+    __typename?: 'Character';
+  };
+}
+
+export interface SetDeathMovesVars {
+  gameRoleId: string;
+  characterId: string;
+  moveNames: string[];
+}
+
+export const getSetDeathMovesOR = (character: Character, moveNames: string[]): SetDeathMovesData => {
+  const optimisticCharacterMoves: SetDeathMovesData_CharacterMove[] = [];
+
+  moveNames.forEach((newMoveName) => {
+    let match = false;
+    character.deathMoves.forEach((oldMove) => {
+      if (oldMove.name === newMoveName) {
+        const optimisticMove: SetDeathMovesData_CharacterMove = {
+          id: oldMove.id,
+          name: oldMove.name,
+          isSelected: true,
+          kind: oldMove.kind,
+          description: oldMove.description,
+          playbook: oldMove.playbook || character.playbook,
+          __typename: 'CharacterMove',
+        };
+
+        optimisticCharacterMoves.push(optimisticMove);
+        match = true;
+      }
+    });
+
+    if (!match) {
+      const optimisticMove: SetDeathMovesData_CharacterMove = {
+        id: 'temp-id',
+        name: newMoveName,
+        isSelected: true,
+        kind: MoveType.character,
+        description: '',
+        playbook: character.playbook,
+        __typename: 'CharacterMove',
+      };
+
+      optimisticCharacterMoves.push(optimisticMove);
+    }
+  });
+
+  return {
+    setDeathMoves: {
+      id: character.id,
+      playbook: character.playbook,
+      characterMoves: optimisticCharacterMoves,
+    },
+  };
+};
+
+const SET_DEATH_MOVES = gql`
+  mutation SetDeathMoves($gameRoleId: String!, $characterId: String!, $moveNames: [String]!) {
+    setDeathMoves(gameRoleId: $gameRoleId, characterId: $characterId, moveNames: $moveNames) {
+      id
+      playbook
+      characterMoves {
+        id
+        isSelected
+        name
+        kind
+        description
+        playbook
+      }
+    }
+  }
+`;
+
+export default SET_DEATH_MOVES;
