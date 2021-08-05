2a73bcb6e0b845087e4326c668b592c70f21d6a7
Michael Mersiades <fakename014@gmail.com>
2021-08-05 13:52:43 +1000

    Restores Keycloak frame checking

--- src/Root.tsx
+++ src/Root.tsx
@@ -16,6 +16,9 @@ interface RootProps {
   children: JSX.Element;
 }
 
+// @ts-ignore istanbul ignore else
+const shouldCheckFrame = window.Cypress ? false : true;
+
 /**
  * This component wraps <App/> with all the Providers it needs.
  */
@@ -26,14 +29,15 @@ const Root: FC<RootProps> = ({ children }) => {
         <FontsProvider>
           <FontsConsumer>
             {(context) => {
-              // The Grommet theme needs the font values, so using the Consumer here immediately.
+              // The Grommet theme needs the font values,
+              // so using the Consumer here immediately.
               return (
                 <Grommet theme={theme(context.vtksReady)} full>
                   <ReactKeycloakProvider
                     authClient={keycloak}
                     initOptions={{
                       onLoad: 'login-required',
-                      checkLoginIframe: false,
+                      checkLoginIframe: shouldCheckFrame,
                     }}
                   >
                     <GameProvider>
